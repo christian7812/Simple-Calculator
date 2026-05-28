74907dc9afb473cb6f03a5380e23df96700a390f
Christian Cruiser <christiancruiseratsagba@gmail.com>
2026-05-28 22:17:11 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Digital Calculator
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function calculate() {
+    try{
+        
+        display.value = eval(display.value);
+
+    } catch(error) {
+        display.value = "Syntax error";
+    }
+ 
+}
+
+function clearDisplay() {
+  display.value = "";
+}
